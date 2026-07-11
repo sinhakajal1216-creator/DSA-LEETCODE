a200b4340254da27cd6e997467f5757ce42ad9bc
kajal <sinhakajal1216@gmail.com>
2026-07-11 08:47:55 +0530

    Added LeetCode 9 - Palindrome Number

---
+++ MATHS/9-PALINDROME.cpp
@@ -0,0 +1,19 @@
+class Solution {
+
+public:
+    bool isPalindrome(int x) {
+    
+       if(x<0){
+        return false;
+       }
+       if(x!=0 && x%10==0){
+        return false;
+       }
+       int rev=0;
+       while(rev<x){
+        rev=rev*10+x%10;
+        x=x/10;
+       }
+        return (x==rev ||x==rev/10);
+    }
+};
